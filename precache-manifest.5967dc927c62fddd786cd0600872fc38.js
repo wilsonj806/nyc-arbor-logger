bc6203b75b7cac8cf2b5c537aeba2af6b650ce95
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-04 17:51:59 +0000

    Deploy wilsonj806/nyc-arbor-logger to github.com/wilsonj806/nyc-arbor-logger.git:gh-pages

---
+++ precache-manifest.5967dc927c62fddd786cd0600872fc38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66398d03f9b170e26221fa1ecedbb457",
+    "url": "/nyc-arbor-logger/index.html"
+  },
+  {
+    "revision": "30c2a9234e2b1f6ec4b1",
+    "url": "/nyc-arbor-logger/static/css/main.e4552159.chunk.css"
+  },
+  {
+    "revision": "85a9f0a374bf2d245946",
+    "url": "/nyc-arbor-logger/static/js/2.1fa51eb0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/nyc-arbor-logger/static/js/2.1fa51eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c2a9234e2b1f6ec4b1",
+    "url": "/nyc-arbor-logger/static/js/main.fe52cc67.chunk.js"
+  },
+  {
+    "revision": "5c53732b4fc70c804e01",
+    "url": "/nyc-arbor-logger/static/js/runtime-main.9cb4e58d.js"
+  }
+]);
